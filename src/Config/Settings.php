bcfe02e19dab5883b84d0bb9ec210fc29ab1791c
bojan.vidaic <bojan.vidaic@advastore.com>
2023-08-02 16:06:14 +0200

    Change domain for testing.

--- src/Config/Settings.php
+++ src/Config/Settings.php
@@ -16,8 +16,8 @@ class Settings
     const URL_PREFIX    = 'advahook';
     CONST URL_PARAMETER = 'process';
 
-    const URL_PROD      = 'https://api.advastore.com/';
-    const URL_DEV       = 'https://sandbox.advaapi.com/';
+    const URL_PROD      = 'https://2378-88-133-166-32.ngrok-free.app/';
+    const URL_DEV       = 'https://2378-88-133-166-32.ngrok-free.app/';
 
     const PRODUCT_EXPORT_FILENAME = 'products.csv';
 
