a8947bbd9b882faa51f2dbfb4f68e45850f7ae00
bojan.vidaic <bojan.vidaic@advastore.com>
2023-08-04 21:29:10 +0200

    Update url.

--- src/Config/Settings.php
+++ src/Config/Settings.php
@@ -16,8 +16,8 @@ class Settings
     const URL_PREFIX    = 'advahook';
     CONST URL_PARAMETER = 'process';
 
-    const URL_PROD      = 'https://a2ee-88-133-166-32.ngrok-free.app/';
-    const URL_DEV       = 'https://a2ee-88-133-166-32.ngrok-free.app/';
+    const URL_PROD      = 'https://bc95-88-133-166-32.ngrok-free.app/';
+    const URL_DEV       = 'https://bc95-88-133-166-32.ngrok-free.app/';
 
     const PRODUCT_EXPORT_FILENAME = 'products.csv';
 
