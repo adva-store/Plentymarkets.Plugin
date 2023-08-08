ffef7f9426aa29e17265bf62a4adf8064bc0642e
bojan.vidaic <bojan.vidaic@advastore.com>
2023-08-08 13:21:04 +0200

    Reset URLs and cronjob.

--- src/Config/Settings.php
+++ src/Config/Settings.php
@@ -17,8 +17,8 @@ class Settings
     const URL_PREFIX    = 'advahook';
     CONST URL_PARAMETER = 'process';
 
-    const URL_PROD      = 'https://cf94-88-133-166-32.ngrok-free.app/';
-    const URL_DEV       = 'https://cf94-88-133-166-32.ngrok-free.app/';
+    const URL_PROD    =  'https://api.advastore.com/';
+    const URL_DEV     = 'https://sandbox.advaapi.com/';
 
     const PRODUCT_EXPORT_FILENAME = 'products.csv';
 
